e47a87a8b3c2c6bb98d6c5e0095d22bfaadd85a6
Samet Demirezen <sametdmzn@gmail.com>
2025-08-11 12:15:17 +0300

    filter button name changed

--- src/components/ResultChecker.js
+++ src/components/ResultChecker.js
@@ -46,7 +46,7 @@ return (
           id="4"
           className="bg-[#0173b9] px-6 py-2 rounded-full text-white h-[40px] lg:w-[146px] lg:h-[40px] md:w-[140px] md:h-[40px]"
         >
-          Filter
+          Sonuçlar
         </button>
       </div>
     </div>
