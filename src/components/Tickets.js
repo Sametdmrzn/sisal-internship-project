4691cd5c88f983c7bf58585332f651571cad4fef
Samet Demirezen <sametdmzn@gmail.com>
2025-08-13 11:20:23 +0300

    Tickets created, resaultchecker deleted again

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -16,6 +16,57 @@ function Tickets(){
         setError("data could not be received");
       });
   }, []);
+
+  return (
+    <div>
+      {games.map((item, index) => {
+        const gameCode = item.next.drawDetail.attributes.find(attr => attr.name === "static.game.acronym")?.value || "Bilinmiyor";
+        const lastDrawNumber = item.last.drawDetail.drawId.number;
+        const lastDrawDate = new Date(item.last.drawDetail.drawDate).toLocaleDateString();
+        const lastDrawTime = new Date(item.last.drawDetail.drawDate).toLocaleTimeString();
+        const nextDrawNumber = item.next.drawDetail.drawId.number;
+        const nextJackpot = item.last.drawDetail.attributes.find(attr => attr.name === "jackpot")?.value;
+
+
+        return (
+          <div className="flex bg-white mx-auto w-[1024px] border-solid border-2 my-2 p-3 shadow-lg rounded-sm" key={index}>
+            <div className=''>
+              <span>{gameCode}</span>
+            </div>
+
+            <div>
+              <div>
+                <div>
+                  Çekiliş No
+                  <div>{lastDrawNumber}</div>
+                </div>
+                <span>{lastDrawDate}</span>
+                <span> -- </span>
+                <span>{lastDrawTime}</span>
+              </div>
+
+              <div>
+                
+                {item.last.drawDetail.winningNumbers?.map(w => w.numbers?.map(item => <span className='bg-black rounded-full text-white'>{item}</span>))}
+
+              </div>
+
+              <div>DETAYLAR</div>
+            </div>
+
+            <div>
+              <div>
+                SIRADAKİ ÇEKİLİŞ <span>{nextDrawNumber}</span>
+              </div>
+              <span>{nextJackpot}</span>
+              <div>HEMEN OYNA</div>
+            </div>
+          </div>
+        );
+
+      })}
+    </div>
+  );
   
 }
 
