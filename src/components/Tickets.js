561e197e915697328a2cb46636a09ec38f014be4
Samet Demirezen <sametdmzn@gmail.com>
2025-08-15 15:52:08 +0300

    Added dynamic background for numbers

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -47,6 +47,7 @@ function Tickets({ activeButton }) {
           (btn) => btn.default.code === gameCode || btn.active.code === gameCode
         );
         const ticketBg = gameButton?.active.bgbanner;
+        const numberBg = gameButton?.active.bg;
         const lastDrawNumber = item.last.drawDetail.drawId.number;
         const lastDrawDate = new Date(
           item.last.drawDetail.drawDate
@@ -90,7 +91,7 @@ function Tickets({ activeButton }) {
                   winning.numbers.map((num, j) => (
                     <span
                       key={`${i}-${j}`}
-                      className="flex items-center justify-center bg-[#ea0029] rounded-full text-white text-[19px] px-2 py-1 h-[35px] w-[35px] mt-2"
+                      className={`flex items-center  bg-gradient-to-b ${numberBg} justify-center rounded-full text-white text-[19px] px-2 py-1 h-[35px] w-[35px] mt-2`}
                     >
                       {num}
                     </span>
