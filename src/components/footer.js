538134ce43d3f81060c83fdf97398ebd7b1e7b43
Samet Demirezen <sametdmzn@gmail.com>
2025-08-21 17:19:25 +0300

    Footer responsive css provided for md breakpoint

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,29 +3,25 @@ import React from "react";
 function footer() {
   return (
     //helpbox-container
-    <div
-      id="helpbox-home"
-      className="bg-[#005c95] flex py-[15px] pl-[30px] pr-[15px]"
-    >
-      <div id="helpbox-container" className="flex-col relative w-full h-full">
-        <div className="text-white text-[19px] mb-[20px] leading-[36px] self-baseline font-medium tracking-[-0.4px]">
+    <div className="bg-[#005c95] flex lg:flex-row py-[15px] pl-[30px] pr-[15px] md:mx-[30px] md:w-auto md:rounded-lg md:m-[30px] ">
+      <div className="flex-col relative w-full h-full ">
+        <div className="text-white text-[19px] sm:text-[32px] mb-[20px] leading-[36px] self-baseline font-medium tracking-[-0.4px] ">
           Yardıma mı ihtiyacın var?
         </div>
-        <div id="helpbox-categories" className="relative m-auto">
+        <div className="relative mx-auto flex flex-col md:flex-row md:gap-8">
           <div className="mb-[4px]">
             <div className="flex md:flex-col items-start">
               <img
                 className="mr-[10px] h-[24px] w-[24px]"
                 src="/assets/MainContent-Assets/phone.svg"
               ></img>
-              <div className="flex-col ">
+              <div>
                 <div className="text-white font-medium">Destek</div>
                 <div className="text-white text-[14px]">0850 390 73 33</div>
               </div>
             </div>
           </div>
           <div>
-            {" "}
             <div className="mb-[4px]">
               <div className="flex md:flex-col items-start">
                 <img
