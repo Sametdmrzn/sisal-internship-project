d00e49ea552a064871df4f7a983638624f317213
Samet Demirezen <sametdmzn@gmail.com>
2025-08-22 16:16:57 +0300

    Create footer layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,55 +2,56 @@ import React from "react";
 
 function footer() {
   return (
-    //helpbox-container
-    <div className="bg-[#005c95] flex lg:flex-row py-[15px] pl-[30px] pr-[15px] md:mx-[30px] md:w-auto md:rounded-lg md:m-[30px] ">
-      <div className="flex-col relative w-full h-full ">
-        <div className="text-white text-[19px] sm:text-[32px] mb-[20px] leading-[36px] self-baseline font-medium tracking-[-0.4px] ">
-          Yardıma mı ihtiyacın var?
-        </div>
-        <div className="relative mx-auto flex flex-col md:flex-row md:gap-8">
-          <div className="mb-[4px]">
-            <div className="flex md:flex-col items-start">
-              <img
-                className="mr-[10px] h-[24px] w-[24px]"
-                src="/assets/MainContent-Assets/phone.svg"
-              ></img>
-              <div>
-                <div className="text-white font-medium">Destek</div>
-                <div className="text-white text-[14px]">0850 390 73 33</div>
-              </div>
-            </div>
+    <>
+      <div className="relative bg-[#005c95] flex lg:flex-col md:mx-[30px] md:w-[980px] md:rounded-[20px] md:h-[195.03px] lg:h-[130px] lg:mx-auto bottom-[-65px]">
+        <div className="flex-col relative w-full h-full py-[15px] pl-[30px] pr-[15px]">
+          <div className="flex flex-col lg:flex-row text-white text-[19px] sm:text-[32px] mb-[20px] leading-[36px] font-medium tracking-[-0.4px] ">
+            Yardıma mı ihtiyacın var?
           </div>
-          <div>
+          <div className="relative mx-auto flex flex-col md:flex-row lg:flex-row md:gap-8">
             <div className="mb-[4px]">
               <div className="flex md:flex-col items-start">
                 <img
                   className="mr-[10px] h-[24px] w-[24px]"
-                  src="/assets/MainContent-Assets/mail.svg"
+                  src="/assets/MainContent-Assets/phone.svg"
                 ></img>
-                <div className="flex-col ">
-                  <div className="text-white font-medium">E-posta</div>
-                  <div className="text-white text-[14px]">
-                    destek@millipiyangoonline.com
+                <div>
+                  <div className="text-white font-medium">Destek</div>
+                  <div className="text-white text-[14px]">0850 390 73 33</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mb-[4px]">
+                <div className="flex md:flex-col items-start">
+                  <img
+                    className="mr-[10px] h-[24px] w-[24px]"
+                    src="/assets/MainContent-Assets/mail.svg"
+                  ></img>
+                  <div className="flex-col ">
+                    <div className="text-white font-medium">E-posta</div>
+                    <div className="text-white text-[14px]">
+                      destek@millipiyangoonline.com
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div>
-            <div className="mb-[4px]">
-              <div className="flex md:flex-col items-start">
-                <img
-                  className="mr-[10px] h-[24px] w-[24px]"
-                  src="/assets/MainContent-Assets/faq.svg"
-                ></img>
-                <div className="flex-col ">
-                  <div className="text-white font-medium">
-                    Sıkça Sorulan Sorular
-                  </div>
-                  <div className="flex text-white text-[14px]">
-                    İnceleyin
-                    <img src="/assets/MainContent-Assets/Arrow-Small-Right-White.svg"></img>
+            <div>
+              <div className="mb-[4px]">
+                <div className="flex md:flex-col items-start">
+                  <img
+                    className="mr-[10px] h-[24px] w-[24px]"
+                    src="/assets/MainContent-Assets/faq.svg"
+                  ></img>
+                  <div className="flex-col ">
+                    <div className="text-white font-medium">
+                      Sıkça Sorulan Sorular
+                    </div>
+                    <div className="flex text-white text-[14px]">
+                      İnceleyin
+                      <img src="/assets/MainContent-Assets/Arrow-Small-Right-White.svg"></img>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -58,7 +59,131 @@ function footer() {
           </div>
         </div>
       </div>
-    </div>
+      <div className="flex flex-col bg-gradient-to-r from-[#005c95] to-[#0d9bf2] items-center p-4">
+        <img
+          className="absolute bottom-0 h-[558px] w-[523px] bottom-auto "
+          src="/assets/MainContent-Assets/mpicontrans.avif"
+        ></img>
+        <div className="w-[950px] mt-[45px] mb-[35px]">
+          <img src="/assets/MainContent-Assets/milli-logo.svg"></img>
+        </div>
+        <div className="flex flex-col w-[950px] max-w-[980px]  ">
+          <div className="flex flex-row  items-start justify-center">
+            <div className="mx-[10px]">
+              <p className="font-bold text-white text-[16px] leading-[20px] mb-[5px]">
+                Sisal Şans
+              </p>
+              <ul className="list-none text-white">
+                <li>Biz Kimiz</li>
+                <li>Üyelik Sözleşmesi</li>
+                <li>Sorumlu Oyun</li>
+                <li>Güvenlik Açığı Bildirimi</li>
+                <li>Blog</li>
+                <li>İkramiye Ödemeleri</li>
+                <li>Çekilişler hakkında</li>
+                <li>Bilgi Edinme Başvuruları</li>
+                <li>Üye Aydınlatma Metni</li>
+                <li>Çerez Politikası</li>
+              </ul>
+            </div>
+            <div className="mx-[10px] pl-[50px]">
+              <p className="font-bold text-white text-[16px] leading-[20px] mb-[5px]">
+                Oyunlar
+              </p>
+              <ul className="list-none text-white ">
+                <li>Milli Piyango</li>
+                <li>Kazi Kazan</li>
+                <li>Sanal Oyunlar</li>
+                <li>Çılgın Sayısal Loto</li>
+                <li>Süper Loto</li>
+                <li>Şans Topu</li>
+                <li>On Numara</li>
+                <li>Hızlı On</li>
+                <li>Hızlı Oyna Hızlı</li>
+                <li>Kazan</li>
+              </ul>
+            </div>
+            <div className="mx-[10px] pl-[50px]">
+              <p className="font-bold text-white text-[16px] leading-[20px] mb-[5px]">
+                Çekiliş Sonuçları
+              </p>
+              <ul className="list-none text-white">
+                <li>Yılbaşı Çekiliş</li>
+                <li>Sonuçları</li>
+                <li>Milli Piyango Sonuçları</li>
+                <li>Sayısal Loto Sonuçları</li>
+                <li>Süper Loto Sonuçları</li>
+                <li>Şans Topu Sonuçları</li>
+                <li>On Numara Sonuçları</li>
+                <li>Hızlı On Sonuçları</li>
+              </ul>
+            </div>
+            <div className="mx-[10px] pl-[50px]">
+              <p className="font-bold text-white text-[16px] leading-[20px] mb-[5px]">
+                Kurallar
+              </p>
+              <ul className="list-none text-white">
+                <li>Milli Piyango</li>
+                <li>Kurallar</li>
+                <li>Çılgın Sayısal Loto</li>
+                <li>Kurallar</li>
+                <li>Süper Loto Kurallar</li>
+                <li>Şans Topu Kurallar</li>
+                <li>On Numara Kurallar</li>
+                <li>Hızlı On Kurallar</li>
+                <li>Kazı Kazan Kurallar</li>
+                <li>Hızlı Oyna Hızlı</li>
+                <li>Kazan Kurallar</li>
+              </ul>
+            </div>
+            <div className="mx-[10px] pl-[50px]">
+              <p className="font-bold text-white text-[16px] leading-[20px] mb-[5px]">
+                Sosyal Medya
+              </p>
+              <div>
+                <ul className="flex list-none text-white items-left">
+                  <li>
+                    <img
+                      src="/assets/MainContent-Assets/Tv.svg"
+                      className="w-[32px] h-[33px]"
+                    ></img>
+                  </li>
+                  <li>
+                    <img
+                      src="/assets/MainContent-Assets/Tv.svg"
+                      className="w-[32px] h-[33px]"
+                    ></img>
+                  </li>
+                  <li>
+                    <img
+                      src="/assets/MainContent-Assets/Tv.svg"
+                      className="w-[32px] h-[33px]"
+                    ></img>
+                  </li>
+                </ul>
+              </div>
+              <div className="text-white font-bold mb-[15px]">
+                Uygulamamıza göz atın
+              </div>
+              <div className="store-buttons-container flex flex-wrap gap-[8px] w-[212px]">
+                <div className="flex items-center justify-center h-[32px] w-[100px] leading-[10px] bg-black rounded-[8px]">
+                  <div className="text-white">App Store</div>
+                </div>
+                <div className="flex items-center justify-center h-[32px] w-[100px] leading-[10px] bg-black rounded-[8px]">
+                  <div className="text-white">Google Play</div>
+                </div>
+                <div className="flex items-center justify-center h-[32px] w-[100px] leading-[10px] bg-black rounded-[8px]">
+                  <div className="text-white">Huawei Store</div>
+                </div>
+                <div className="flex items-center justify-center h-[32px] w-[100px] leading-[10px] bg-black rounded-[8px]">
+                  <div className="text-white">Galaxy Store</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
